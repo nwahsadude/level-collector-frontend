a830630814c8df3f0a9e872493cbf5ddf33859e1
nwahsadude <nwahsadude@gmail.com>
2020-03-08 09:21:35 -0500

    Updates

---
+++ precache-manifest.968b05dfd99028772d764b4f60843b1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee850b5c674eb8dda31fd2e6fe2e6e76",
+    "url": "/level-collector-frontend/index.html"
+  },
+  {
+    "revision": "46e17573b2e787ba4bd9",
+    "url": "/level-collector-frontend/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "a217ab34747f23f091eb",
+    "url": "/level-collector-frontend/static/css/main.9beffc53.chunk.css"
+  },
+  {
+    "revision": "46e17573b2e787ba4bd9",
+    "url": "/level-collector-frontend/static/js/2.f8e6c943.chunk.js"
+  },
+  {
+    "revision": "4ef169e897079a10287750a24d31d242",
+    "url": "/level-collector-frontend/static/js/2.f8e6c943.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a217ab34747f23f091eb",
+    "url": "/level-collector-frontend/static/js/main.48ecf6b1.chunk.js"
+  },
+  {
+    "revision": "f8b78facb8f3ff4b0636",
+    "url": "/level-collector-frontend/static/js/runtime-main.2196c069.js"
+  }
+]);
